6afa736af0e78dab050f9d22c10321cfadb152dd
v-ashka <mwijaszka99@gmail.com>
2024-12-10 14:15:21 +0100

    changed node dependencies for nodejs v18.20.5

--- _dev/import/index.js
+++ _dev/import/index.js
@@ -12,6 +12,7 @@
 // We keep the old way of importing them for backward compatibility.
 // @see: https://devdocs.prestashop-project.org/1.7/development/components/global-components/
 import Importer from '@pages/import-data/Importer';
+import Uploader from "./Uploader";
 const $ = window.$;
 
 $(() => {
@@ -29,5 +30,37 @@ $(() => {
   const importer = new Importer();
   importer._progressModal = importer.progressModal;
 
-  console.log(importer);
+  const uploader = new Uploader();
+
+  $(document).off('click', '.js-process-import');
+  $(document).on('click', '.js-process-import', (e) => importHandler(e));
+
+  $(document).on('click', '.js-abort-import', () => importer.requestCancelImport());
+  $(document).on('click', '.js-close-modal', () => {
+    importer.progressModal.hide()
+    location.reload()
+  });
+  $(document).on('click', 'js-continue-import', () => importer.continueImport());
+  $(document).on('change', '.js-import-file', () => {uploader.uploadFile()})
+  function importHandler(e){
+    e.preventDefault();
+
+    let configuration = {};
+
+    // Collect the configuration from the form into an array.
+    $('#import-features-product-form').find(
+      'input[name=skip]:checked, select[name^=type_value], #csv, #iso_lang, #entity,' +
+      '#truncate, #match_ref, #regenerate, input[name=forceIDs]:checked, #sendemail,' +
+      '#separator, #multiple_value_separator, #price_tin'
+    ).each((index, $input) => {
+      configuration[$($input).attr('name')] = $($input).val();
+    });
+    configuration['import_type'] = parseInt($('#import_type')[0].value)
+    importer.import(
+      $(e.currentTarget).data('import_url'),
+      configuration
+    )
+
+  }
+
 });
